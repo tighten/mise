77810ba2c5768c527c5b04924aa4033e3555a657
Jon Sugar <jonathansugar@mac.com>
2025-03-20 13:10:08 +0000

    Resolve Steps by classname

--- app/Recipes/Tighten/BasicSaas.php
+++ app/Recipes/Tighten/BasicSaas.php
@@ -3,17 +3,18 @@
 namespace App\Recipes\Tighten;
 
 use App\Recipes\Recipe;
+use App\Steps\Duster\Install as DusterInstall;
 
 class BasicSaas extends Recipe
 {
     public function __invoke(): void
     {
-        $this->step('duster/install');
-        $this->step('duster/ci', someParameterHereOrWhatever: true);
+        $this->step(DusterInstall::class);
 
-        if ($this->confirm(label: 'Do you want to install our frontend tooling?')) {
-            $this->step('tighten/prettier');
-        }
+        // $this->step('duster/ci', someParameterHereOrWhatever: true);
+        // if ($this->confirm(label: 'Do you want to install our frontend tooling?')) {
+        //     $this->step('tighten/prettier');
+        // }
     }
 
     public function name(): string
