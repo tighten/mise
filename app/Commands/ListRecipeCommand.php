f0fada02cc4d62c3d7cb3d6336b56e9690b51b41
Jon Sugar <jonathansugar@mac.com>
2025-03-24 08:52:33 +0000

    Improve code readability.

--- app/Commands/ListRecipeCommand.php
+++ app/Commands/ListRecipeCommand.php
@@ -14,24 +14,26 @@ class ListRecipeCommand extends Command
 
     public function handle(): void
     {
-        $recipes = collect(config('mise.recipes'));
-
-        $padding = $recipes->keys()->max(fn ($recipe) => strlen($recipe) + 4);
-
-        $recipeList = $recipes->reduce(function (?string $carry, string $recipeClass, $key) use ($padding) {
-            return sprintf($carry . "  <info>%-{$padding}s</info> %s\n", $key, app($recipeClass)->description());
-        });
-
         $this->newLine();
-        $this->line(sprintf('Mise <info>v%s</info>', config('app.version')));
+        $this->line('Mise <info>v' . config('app.version') . '</info> ');
         $this->newLine();
         $this->line('<fg=yellow>Recipies:</>');
-        $this->line($recipeList);
+        $this->line($this->recipes());
         $this->line('<fg=yellow>Applying recipes:</>');
         $this->line('    mise apply [recipies]');
         $this->newLine();
         $this->line('<fg=yellow>Examples:</>');
         $this->line('    mise apply tighten/basic-sass');
         $this->line('    mise apply tighten/basic-sass laravel/local-developer-tooling');
+    }
+
+    private function recipes(): string
+    {
+        $recipes = collect(config('mise.recipes'));
+        $padding = $recipes->keys()->max(fn ($recipe) => strlen($recipe) + 4);
+
+        return $recipes->reduce(
+            fn ($carry, $recipeClass, $key) => sprintf("%s  <info>%s</info> %s\n", $carry, sprintf("%-{$padding}s", $key), app($recipeClass)->description())
+        );
     }
 }
