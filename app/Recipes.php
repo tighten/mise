b7252af496ed10ecf69a2d9cce84d35898b0ecdd
Matt Stauffer <matt@tighten.co>
2025-03-28 18:04:24 -0400

    Pull in local recipes in ~/.mise/Recipes

--- app/Recipes.php
+++ app/Recipes.php
@@ -4,16 +4,22 @@ namespace App;
 
 use App\Recipes\Recipe;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\File;
 
 class Recipes
 {
     public function all(): Collection
     {
-        return collect(File::files(app_path('Recipes')))
-            ->map(fn ($file) => "App\\Recipes\\" . pathinfo($file, PATHINFO_FILENAME))
-            ->filter(fn ($class) => class_exists($class) && is_subclass_of($class, Recipe::class))
-            ->mapWithKeys(fn ($class) => [(new $class)->slug() => $class]);
+        $customRecipesDir = $_SERVER['HOME'] . '/.mise/Recipes';
+
+        if (is_dir($customRecipesDir)) {
+            $this->loadFilesInPath($customRecipesDir);
+        }
+
+        return $this->allInPath(app_path('Recipes'))->when(is_dir($customRecipesDir), function (Collection $recipes) use ($customRecipesDir){
+            return $recipes->merge($this->allInPath($customRecipesDir));
+        });
     }
 
     public function allForSelect(): array
@@ -28,5 +34,20 @@ class Recipes
     public function keys(): array
     {
         return $this->all()->keys()->toArray();
+    }
+
+    protected function allInPath(string $path): Collection
+    {
+        return collect(File::files($path))
+            ->map(fn ($file) => "App\\Recipes\\" . pathinfo($file, PATHINFO_FILENAME))
+            ->filter(fn ($class) => class_exists($class) && is_subclass_of($class, Recipe::class))
+            ->mapWithKeys(fn ($class) => [(new $class)->slug() => $class]);
+    }
+
+    protected function loadFilesInPath(string $path): void
+    {
+        collect(File::files($path))
+            ->filter(fn ($fileName) => Str::endsWith($fileName, '.php'))
+            ->map(fn ($fileName) => require $fileName);
     }
 }
