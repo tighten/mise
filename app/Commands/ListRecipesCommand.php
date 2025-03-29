4640e9f57557c8a0f423d56d1524c7e04a503578
Jon Sugar <jonathansugar@mac.com>
2025-03-29 09:42:23 +0000

    Use consistent colors, font weight and spacing as zero's default command summary renderer.

--- app/Commands/ListRecipesCommand.php
+++ app/Commands/ListRecipesCommand.php
@@ -14,14 +14,14 @@ class ListRecipesCommand extends Command
     public function handle(): void
     {
         $this->newLine();
-        $this->line('Mise <info>' . config('app.version') . '</info> ');
+        $this->line('  <fg=white;options=bold>Mise</>  <fg=green;options=bold>' . config('app.version') . '</> ');
         $this->newLine();
-        $this->line('<fg=yellow>Recipes:</>');
+        $this->line('  <fg=yellow;options=bold>Recipes:</>');
         $this->line($this->recipes());
-        $this->line('<fg=yellow>Applying recipes:</>');
+        $this->line('  <fg=yellow;options=bold>Applying recipes:</>');
         $this->line('    mise apply [recipes]');
         $this->newLine();
-        $this->line('<fg=yellow>Examples:</>');
+        $this->line('  <fg=yellow;options=bold>Examples:</>');
         $this->line('    mise apply tighten-basic-saas');
         $this->line('    mise apply tighten-basic-saas laravel-local-developer-tooling');
     }
@@ -32,7 +32,7 @@ class ListRecipesCommand extends Command
         $padding = $recipes->keys()->max(fn ($recipe) => strlen($recipe) + 4);
 
         return $recipes->reduce(
-            fn ($carry, $recipeClass, $key) => sprintf("%s  <info>%s</info> %s\n", $carry, sprintf("%-{$padding}s", $key), app($recipeClass)->description())
+            fn ($carry, $recipeClass, $key) => sprintf("%s  <info>%s</info> %s\n", $carry, sprintf("  %-{$padding}s", $key), app($recipeClass)->description())
         );
     }
 }
