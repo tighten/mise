1b532ae6cfe19b2be8f08ed275f6ac8a0c2c0a51
Jon Sugar <jonathansugar@mac.com>
2025-05-05 12:54:13 +0100

    remove unused import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\DevelopmentCommands\DusterFixCommand;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +13,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
     /**
      * Register any application services.
      */
