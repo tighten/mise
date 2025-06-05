f4e74a524fb9b7ca38513ba1a7af36ef18c272b7
Jon Sugar <jonathansugar@mac.com>
2025-06-05 13:49:19 +0100

    add methods to inspect composer dependencies

--- app/Tools/Composer.php
+++ app/Tools/Composer.php
@@ -2,6 +2,8 @@
 
 namespace App\Tools;
 
+use Illuminate\Support\Facades\Storage;
+
 class Composer extends ConsoleCommand
 {
     public function requireDev(string $package): static
@@ -23,5 +25,51 @@ class Composer extends ConsoleCommand
         $this->exec("composer remove {$package}");
 
         return $this;
+    }
+
+    public function removeDev(string $string): static
+    {
+        $this->exec("composer remove {$string} --dev");
+
+        return $this;
+    }
+
+    public function hasDevDependency(string $package): bool
+    {
+        $dependencies = $this->getDevelopmentDependencies();
+
+        return isset($dependencies[$package]);
+    }
+
+    public function hasDependency(string $package): bool
+    {
+        $dependencies = array_merge(
+            $this->getProductionDependencies(),
+            $this->getDevelopmentDependencies(),
+        );
+
+        return isset($dependencies[$package]);
+    }
+
+    public function hasProductionDependency(string $package): bool
+    {
+        $dependencies = $this->getProductionDependencies();
+
+        return isset($dependencies[$package]);
+    }
+
+    public function getDevelopmentDependencies(): mixed
+    {
+        return $this->getComposerConfiguration()['require-dev'];
+    }
+
+    public function getProductionDependencies(): mixed
+    {
+        return $this->getComposerConfiguration()['require'];
+    }
+
+    public function getComposerConfiguration(): array
+    {
+        return json_decode(Storage::get('composer.json'), true);
     }
 }
